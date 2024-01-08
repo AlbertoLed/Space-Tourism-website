dbe4390d0996a36d90b3e8029a6c18e598dec333
Alberto Ledesma <alberto.ledesma.dev@gmail.com>
2024-01-08 17:13:05 -0600

    Add Home layout and styles for small screens

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,3 +1,17 @@
 export default function Home() {
-    return <h1>Home page goes here</h1>
+
+    const bgImg = "bg-[url('./home/background-home-mobile.jpg')]"
+
+    return(
+        <header className={`${bgImg} min-h-[100vh] bg-cover text-text grid content-center pt-[60px]`}>
+            <div className="w-[90%] mx-auto flex flex-col space-y-[80px]">
+                <div className="grid justify-items-center space-y-7">
+                    <p className="font-barlow-con uppercase text-lg tracking-widest">So, you want to travel to</p>
+                    <h1 className="text-primary font-bellefair uppercase text-7xl">Space</h1>
+                    <p className="font-barlow text-center text-pretty">Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
+                </div>
+                <button className="bg-primary text-accent text-xl tracking-wider font-bellefair uppercase mx-auto w-[150px] h-[150px] rounded-full">Explore</button>
+            </div>
+        </header>
+    )
 }
