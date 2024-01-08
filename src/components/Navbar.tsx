135f79c83383ec1802ac233db1f9678a5682271d
Alberto Ledesma <alberto.ledesma.dev@gmail.com>
2024-01-07 19:24:34 -0600

    Add a basic layout and functionality for the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,25 @@
+import { useState } from "react"
 import { Outlet } from "react-router-dom"
+import { FaBars } from "react-icons/fa"
+import logo from "../../public/logo.svg"
 
 export default function Navbar() {
+    const [showNav, toggleShowNav] = useState(false)
     return(
         <>
-            <h1>This is the navbar</h1>
+                <div className="absolute w-full">
+                    <div className="flex w-full items-center justify-between">
+                        <img src={logo} alt="Logo" />
+                        <FaBars
+                        className="text-4xl hover:cursor-pointer"
+                          onClick={() => toggleShowNav(prev => !prev)}
+                        />
+                    </div>
+                        {showNav ? 
+                            <nav>This is the navbar</nav>
+                            : null
+                        }
+                </div>
             <Outlet />
         </>
     )
