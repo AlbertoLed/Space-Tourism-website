8775722744be5ba9ce41fa91d0f8b760eef0a69e
Alberto Ledesma <alberto.ledesma.dev@gmail.com>
2024-01-15 17:32:20 -0600

    Add crew data and images

--- src/data.ts
+++ src/data.ts
@@ -1,35 +1,65 @@
 import data from "../data.json"
-import { Destination } from "./utils"
+import { Destination, CrewMember } from "./utils"
 import imgMoon from "./assets/destination/image-moon.png"
 import imgMars from "./assets/destination/image-mars.png"
 import imgEuropa from "./assets/destination/image-europa.png"
 import imgTitan from "./assets/destination/image-titan.png"
+import imgDouglas from "./assets/crew/image-douglas-hurley.png"
+import imgMark from "./assets/crew/image-mark-shuttleworth.png"
+import imgVictor from "./assets/crew/image-victor-glover.png"
+import imgAnousheh from "./assets/crew/image-anousheh-ansari.png"
 
 export const moon: Destination = {
     name: data.destinations[0].name,
     description: data.destinations[0].description,
     distance: data.destinations[0].distance,
     travel: data.destinations[0].travel,
-    image: imgMoon
+    image: imgMoon,
 }
 export const mars: Destination = {
     name: data.destinations[1].name,
     description: data.destinations[1].description,
     distance: data.destinations[1].distance,
     travel: data.destinations[1].travel,
-    image: imgMars
+    image: imgMars,
 }
 export const europa: Destination = {
     name: data.destinations[2].name,
     description: data.destinations[2].description,
     distance: data.destinations[2].distance,
     travel: data.destinations[2].travel,
-    image: imgEuropa
+    image: imgEuropa,
 }
 export const titan: Destination = {
     name: data.destinations[3].name,
     description: data.destinations[3].description,
     distance: data.destinations[3].distance,
     travel: data.destinations[3].travel,
-    image: imgTitan
+    image: imgTitan,
 }
+
+export const douglas: CrewMember =  {
+    name: data.crew[0].name,
+    role: data.crew[0].role,
+    bio: data.crew[0].bio,
+    image: imgDouglas,
+}
+export const mark: CrewMember =  {
+    name: data.crew[1].name,
+    role: data.crew[1].role,
+    bio: data.crew[1].bio,
+    image: imgMark,
+}
+export const victor: CrewMember =  {
+    name: data.crew[2].name,
+    role: data.crew[2].role,
+    bio: data.crew[2].bio,
+    image: imgVictor,
+}
+export const anousheh: CrewMember =  {
+    name: data.crew[3].name,
+    role: data.crew[3].role,
+    bio: data.crew[3].bio,
+    image: imgAnousheh,
+}
+console.log(data.crew)
--- src/utils.ts
+++ src/utils.ts
@@ -7,3 +7,10 @@ export interface Destination {
     travel: string;
     image: string;
 }
+
+export interface CrewMember {
+    name: string;
+    role: string;
+    bio: string;
+    image: string;
+}
